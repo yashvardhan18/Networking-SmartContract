55f6dd5c8cc5f662c1091d5dc4108423da5a97ba
yashvardhan18 <vardhanyash20000@gmail.com>
2024-01-22 15:50:13 +0530

    Logic added for roboincome plan commencing testing for second income plan & retesting for first income plan

--- contracts/Networking.sol
+++ contracts/Networking.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "hardhat/console.sol";
+
+// import "hardhat/console.sol";
 
 contract Networking is OwnableUpgradeable {
     struct UserDetails {
@@ -14,14 +15,16 @@ contract Networking is OwnableUpgradeable {
         uint256 claimedIncome;
         uint256 withdrawedReward;
         uint256 totalReward;
-        uint256 token;
-        address referrerAddress;
-        uint8 rank;
+        uint256 roboReward;
         uint256 depositCount;
+        uint256 roboCount;
+        uint256 roboSubscriptionPeriod;
+        uint8 rank;
+        address referrerAddress;
     }
 
     address public stakeToken;
-    address public rewardWallet;
+    address public companyRoboRewardWallet;
     uint256 serviceChargeAmount;
     uint128 public ROIpercent;
     uint128 public ROITime;
@@ -30,10 +33,10 @@ contract Networking is OwnableUpgradeable {
     uint128 public minWithdrawalAmount;
 
     mapping(address => uint256) public referralIncome;
-    mapping(address => uint256) public claimedROIIncome;
     mapping(address => UserDetails) public Details;
     mapping(uint8 => mapping(uint8 => uint128)) public packageThreshold;
-    mapping(address => uint8) public referrerCount;
+    mapping(uint8 => uint256) roboFee;
+    mapping(uint8 => uint256) roboLevelPercentage;
 
     error ZeroAmount();
     error InvalidRank();
@@ -46,6 +49,7 @@ contract Networking is OwnableUpgradeable {
     error invalidAddress();
     error ROINotReady();
     error InvalidWithdrawalAmount();
+    error InvalidDepositAmount();
 
     event Staked(
         address indexed user,
@@ -59,19 +63,7 @@ contract Networking is OwnableUpgradeable {
         uint256 indexed amount,
         uint256 indexed timestamp
     );
-    event allocatedLevelIncome(
-        address indexed sender,
-        address indexed receiver,
-        uint256 indexed amount,
-        uint256 timestamp
-    );
-    //event for allocatedDirectIncome
-    event allocatedDirectIncome(
-        address indexed sender,
-        address indexed receiver,
-        uint256 indexed amount,
-        uint256 timestamp
-    );
+
     event withdrawIncome(
         address indexed receiver,
         uint256 indexed amount,
@@ -95,9 +87,10 @@ contract Networking is OwnableUpgradeable {
 
     function initialize(
         address _stakeToken,
-        uint8[] memory ranks,
-        uint8[] memory levels,
-        uint128[] memory ROIPercentage,
+        uint8[] memory _ranks,
+        uint8[] memory _levels,
+        uint128[] memory _ROIPercentage,
+        uint128[] memory _roboFee,
         uint128 _ROIPercent
     ) external initializer {
         __Ownable_init(msg.sender);
@@ -105,18 +98,19 @@ contract Networking is OwnableUpgradeable {
         rewardWallet = owner();
         Details[owner()].totalInvestment = 1;
         ROIpercent = _ROIPercent;
-        uint8 levels_length = uint8(levels.length);
-        uint8 ranks_length = uint8(ranks.length);
+        uint8 levels_length = uint8(_levels.length);
+        uint8 ranks_length = uint8(_ranks.length);
         uint8 flag = 0;
         for (uint i = 0; i < ranks_length; i++) {
+            roboFee[i] = _roboFee[i];
             for (uint j = 0; j < levels_length; j++) {
-                packageThreshold[ranks[i]][levels[j]] = ROIPercentage[flag];
+                packageThreshold[_ranks[i]][_levels[j]] = _ROIPercentage[flag];
                 flag++;
             }
         }
     }
 
-    function deposit(uint8 _rank, address _referrer, uint256 amount) public {
+    function deposit(uint8 _rank, address _referrer, uint256 amount) external {
         address user = msg.sender;
         uint256 timeNow = block.timestamp;
 
@@ -129,20 +123,15 @@ contract Networking is OwnableUpgradeable {
                 revert InvalidReferrer();
             }
         } else {
-            // _referrer = rewardWallet;
+            _referrer = rewardWallet;
+        }
+
+        if (amount < 100000000 || amount > 25000000000000) {
+            revert InvalidDepositAmount();
         }
 
         if (Details[user].totalInvestment == 0) {
             Details[user].referrerAddress = _referrer;
-
-            referrerCount[_referrer] += 1;
-        }
-
-        if (
-            _referrer != address(0) &&
-            Details[Details[user].referrerAddress].totalInvestment == 0
-        ) {
-            revert InvalidReferrer();
         }
 
         _calculateRewards(user);
@@ -151,23 +140,97 @@ contract Networking is OwnableUpgradeable {
         Details[_referrer].refereeCount += 1;
         Details[user].depositTime = timeNow;
         Details[user].totalInvestment += amount;
+        Details[user].roboCount += 1;
+        Details[user].roboSubscriptionPeriod = block.timestamp;
+        roboRewardDistribution(Details[user].totalInvestment, msg.sender);
         SafeERC20.safeTransferFrom(
             IERC20(stakeToken),
             user,
             address(this),
             amount
         );
-
-        emit Staked(
-            user,
-            Details[user].referrerAddress,
-            timeNow,
-            amount,
-            Details[user].totalInvestment
-        );
     }
 
-    function distributeRI(
+    function roboRewardDistribution(
+        uint256 _investmentAmount,
+        address _user
+    ) internal {
+        uint fee;
+
+        if (
+            Details[_user].roboCount > 1 &&
+            Details[_user].roboCount < 6 &&
+            _investmentAmount >= _investmentAmount * 2
+        ) {
+            fee = 0;
+        } else {
+            if (
+                _investmentAmount >= 100000000 &&
+                _investmentAmount <= 2490000000
+            ) {
+                fee = roboFee[1];
+            } else if (
+                _investmentAmount >= 2500000000 &&
+                _investmentAmount <= 9999000000
+            ) {
+                fee = roboFee[2];
+            } else if (
+                _investmentAmount >= 10000000000 &&
+                _investmentAmount <= 24999000000
+            ) {
+                fee = roboFee[3];
+            } else if (
+                _investmentAmount >= 25000000000 &&
+                _investmentAmount <= 999999000000
+            ) {
+                fee = roboFee[3];
+            } else if (
+                _investmentAmount >= 1000000000000 &&
+                _investmentAmount <= 4999999000000
+            ) {
+                fee = roboFee[4];
+            } else if (
+                _investmentAmount >= 5000000000000 &&
+                _investmentAmount <= 25000000000000
+            ) {
+                fee = roboFee[5];
+            }
+
+            companyRewardWallet += (fee * 7500) / 10000;
+
+            uint distributionAmount = fee - ((fee * 7500) / 10000);
+
+            address[] memory referrers = new address[](4);
+            referrers[0] = Details[_user].referrerAddress;
+            uint8 i;
+
+            for (i = 1; i < 4; i++) {
+                referrers[i] = Details[referrers[i - 1]].referrerAddress;
+                if (referrers[i] == address(0)) {
+                    break;
+                }
+            }
+
+            uint256[] memory rewardShares = new uint256[](numReferrers);
+
+            for (i = 0; i < numReferrers; i++) {
+                if (referrers[i] != address(0)) {
+                    rewardShares[i] =
+                        (_amount * getRoboLevelPercentage(i + 1)) /
+                        10000;
+                    distributionAmount -= rewardShares[i];
+                }
+
+                Details[referrers[i]].roboReward = rewardShares[i];
+            }
+
+            if (distributionAmount > 0) {
+                companyRewardWallet += distributionAmount;
+            }
+        }
+    }
+
+    function distributeROI(
         address _user,
         uint256 _amount
     ) internal returns (uint256) {
@@ -190,7 +253,7 @@ contract Networking is OwnableUpgradeable {
         for (i = 0; i < numReferrers; i++) {
             if (referrers[i] != address(0)) {
                 rewardShares[i] =
-                    (_amount * getAmountPercentage(i + 1, _user)) /
+                    (_amount * getAmountPercentage(i + 1, referrers[i])) /
                     10000;
 
                 remainingReward -= rewardShares[i];
@@ -205,16 +268,22 @@ contract Networking is OwnableUpgradeable {
             );
         }
 
-        Details[msg.sender].totalReward += remainingReward;
+        serviceChargeAmount += remainingReward;
     }
 
     function getAmountPercentage(
         uint8 position,
-        address user
+        address referrer
     ) internal view returns (uint256) {
-        console.log( packageThreshold[Details[user].rank][position]);
+        console.log(packageThreshold[Details[referrer].rank][position]);
 
-        return packageThreshold[Details[user].rank][position];
+        return packageThreshold[Details[referrer].rank][position];
+    }
+
+    function getRoboLevelPercentage(
+        uint8 position
+    ) internal view returns (uint256) {
+        return roboLevelPercentage[position];
     }
 
     function setPackage(
@@ -235,83 +304,109 @@ contract Networking is OwnableUpgradeable {
         return _income;
     }
 
-    function _calculateRewards(address _user) public returns (uint256 reward) {
+    function _calculateRewards(
+        address _user
+    ) internal returns (uint256 reward) {
         UserDetails storage details = Details[_user];
         uint256 time = (block.timestamp - details.depositTime) / 86400;
         uint256 TotalROI = ((Details[_user].totalInvestment *
             time *
             ROIpercent) / 10000);
-    console.log("total ROI", TotalROI);
-        if (TotalROI >= details.totalInvestment * 2) {
-            TotalROI = details.totalInvestment * 2;
 
-            serviceChargeAmount += (TotalROI * 5000) / 10000;
-            reward = TotalROI - (TotalROI * 5000) / 10000;
-        } else {
-            revert ROINotReady();
-        }
+        uint serviceCharge = (TotalROI * 5000) / 10000;
+        reward = TotalROI - (TotalROI * 5000) / 10000;
+        Details[msg.sender].totalReward += referralIncome[msg.sender] + reward;
+        referralIncome[msg.sender] = 0;
 
-        return distributeRI(_user, reward);
+        return distributeROI(_user, serviceCharge);
     }
 
     function setROIPercentage(
         uint128 _percent
-    ) public onlyOwner returns (uint128) {
+    ) external onlyOwner returns (uint128) {
         if (_percent == 0) {
             revert ZeroAmount();
         }
         return ROIpercent = _percent;
     }
 
-    function setROITime(uint128 _time) public onlyOwner returns (uint128) {
+    function setROITime(uint128 _time) external onlyOwner returns (uint128) {
         if (_time == 0) {
             revert ZeroAmount();
         }
         return ROITime = _time;
     }
 
-    function withdrawReward(uint256 _amount) public {
+    function setMinWithdrawalLimit(uint256 _newLimit) external onlyOwner {
+        minWithdrawalAmount = _newLimit;
+    }
+
+    function withdrawReward(uint256 _amount) external {
         UserDetails storage details = Details[msg.sender];
         if (_amount < minWithdrawalAmount) {
             revert BelowMinLimit();
         }
 
-        if (_amount > Details[msg.sender].totalInvestment) {
+        if (_amount > Details[msg.sender].totalReward) {
             revert InvalidWithdrawalAmount();
         }
-        uint256 time = (block.timestamp - details.depositTime) / 86400;
-        uint256 TotalROI = ((Details[msg.sender].totalInvestment *
-            time *
-            ROIpercent) / 10000);
-        if (TotalROI >= details.totalInvestment * 2) {
-            _calculateRewards(msg.sender);
-            Details[msg.sender].totalReward += referralIncome[msg.sender];
-            referralIncome[msg.sender] = 0;
-            Details[msg.sender].depositTime = block.timestamp;
-            console.log("Total reward", Details[msg.sender].totalReward);
-            details.withdrawedReward += _amount;
-            details.totalReward -= _amount;
-            console.log("Total reward 2", Details[msg.sender].totalReward);
-            SafeERC20.safeTransfer(IERC20(stakeToken), msg.sender, _amount);
-        } else if (
-            details.withdrawedReward > 0 && _amount <= details.totalReward
-        ) {
-            Details[msg.sender].totalReward += referralIncome[msg.sender];
-            referralIncome[msg.sender] = 0;
-            Details[msg.sender].depositTime = block.timestamp;
-            console.log("Total reward", Details[msg.sender].totalReward);
-            details.withdrawedReward += _amount;
-            details.totalReward -= _amount;
-            console.log("Total reward 2", Details[msg.sender].totalReward);
-            SafeERC20.safeTransfer(IERC20(stakeToken), msg.sender, _amount);
-        } else {
+
+        if (Details[msg.sender].totalReward < minWithdrawalAmount) {
             revert ROINotReady();
         }
+
+        _calculateRewards(msg.sender);
+
+        Details[msg.sender].depositTime = block.timestamp;
+
+        details.withdrawedReward += _amount;
+        details.totalReward -= _amount;
+
+        SafeERC20.safeTransfer(IERC20(stakeToken), msg.sender, _amount);
 
         emit withdrawIncome(msg.sender, _amount, block.timestamp);
     }
 
-    function seeDeposit(
+    function withdrawRoboIncome(uint256 _amount) external {
+        address _user = msg.sender;
+        if (_amount > Details[_user].roboReward) {
+            revert InvalidWithdrawalAmount();
+        }
+
+        Details[_user].roboReward -= _amount;
+        SafeERC20.safeTransfer(IERC20(stakeToken), _user, _amount);
+    }
+
+    function withdrawInvestment(uint256 _amount) external {
+        address _user = msg.sender;
+        if (_amount > Details[_user].totalInvestment) {
+            revert InvalidWithdrawalAmount();
+        }
+        if (Details[_user].totalReward < Details[_user].totalInvestment) {
+            revert ROINotReady();
+        }
+
+        Details[_user].totalInvestment -= _amount;
+        SafeERC20.safeTransfer(IERC20(token), _user, _amount);
+    }
+
+    function withdrawServiceChargeAmountAndCompanyRoboReward(
+        uint256 _serviceChargeAmount,
+        uint256 _roboRewardAmount
+    ) external onlyOwner {
+        if (_serviceChargeAmount > serviceChargeAmount) {
+            revert InvalidWithdrawalAmount();
+        }
+        if (_roboRewardAmount > companyRoboRewardWallet) {
+            revert InvalidWithdrawalAmount();
+        }
+        serviceChargeAmount -= _serviceChargeAmount;
+        companyRoboRewardWallet -= _roboRewardAmount;
+        SafeERC20.safeTransfer(IERC20(token), owner(), _serviceChargeAmount);
+        SafeERC20.safeTransfer(IERC20(token), owner(), _roboRewardAmount);
+    }
+
+    function seeInvestment(
         address user,
         uint256 depositCount
     ) external view returns (uint256) {
--- test/Lock.ts
+++ test/Lock.ts
@@ -1,13 +1,19 @@
+/** @format */
+
 import { expect } from "chai";
-import { ethers} from "hardhat";
+import { ethers } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers";
-import { Networking, Networking__factory, Usdc, Usdc__factory } from "../typechain-types";
-import { expandTo18Decimals,expandTo6Decimals } from "./utilities/utilities";
+import {
+  Networking,
+  Networking__factory,
+  Usdc,
+  Usdc__factory,
+} from "../typechain-types";
+import { expandTo18Decimals, expandTo6Decimals } from "./utilities/utilities";
 import { usdcSol } from "../typechain-types/contracts";
 import { time } from "@nomicfoundation/hardhat-network-helpers";
 
 describe("Lock", function () {
-  
   let myNetworking: Networking; //Primary marketplace contract
   let mockUsdc: Usdc; //ERC20 fanverse token
   let owner: SignerWithAddress;
@@ -20,62 +26,107 @@ describe("Lock", function () {
     owner = signers[0];
     myNetworking = await new Networking__factory(owner).deploy();
     mockUsdc = await new Usdc__factory(owner).deploy();
-    await myNetworking.connect(owner).initialize(mockUsdc.address,[1,2,3,4,5,6],[1,2,3,4,5,6],[1200,700,500,300,200,100,1300,800,550,350,250,150,1400,900,600,400,300,200,1500,1000,650,450,350,250,1800,1100,700,500,400,300,2000,1200,750,550,450,350],150);
-  
+    await myNetworking
+      .connect(owner)
+      .initialize(
+        mockUsdc.address,
+        [1, 2, 3, 4, 5, 6],
+        [1, 2, 3, 4, 5, 6],
+        [
+          1200, 700, 500, 300, 200, 100, 1300, 800, 550, 350, 250, 150, 1400,
+          900, 600, 400, 300, 200, 1500, 1000, 650, 450, 350, 250, 1800, 1100,
+          700, 500, 400, 300, 2000, 1200, 750, 550, 450, 350,
+        ],
+        150
+      );
   });
 
-  
-
   describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      await mockUsdc.connect(owner).mint(signers[1].address,expandTo6Decimals(3000));
-      await mockUsdc.connect(owner).mint(signers[2].address,expandTo6Decimals(3000));
-      await mockUsdc.connect(owner).mint(signers[3].address,expandTo6Decimals(3000));
-      await mockUsdc.connect(owner).mint(signers[4].address,expandTo6Decimals(3000));
-      await mockUsdc.connect(owner).mint(signers[5].address,expandTo6Decimals(3000));
-      await mockUsdc.connect(owner).mint(signers[6].address,expandTo6Decimals(3000));
-      
-      
-      await mockUsdc.connect(owner).approve(myNetworking.address,expandTo6Decimals(3000));
-      await mockUsdc.connect(signers[1]).approve(myNetworking.address,expandTo6Decimals(3000));
-      await mockUsdc.connect(signers[2]).approve(myNetworking.address,expandTo6Decimals(3000));
-      await mockUsdc.connect(signers[3]).approve(myNetworking.address,expandTo6Decimals(3000));
-      await mockUsdc.connect(signers[4]).approve(myNetworking.address,expandTo6Decimals(3000));
-      await mockUsdc.connect(signers[5]).approve(myNetworking.address,expandTo6Decimals(3000));
-      await mockUsdc.connect(signers[6]).approve(myNetworking.address,expandTo6Decimals(3000));
+    it("Test case for depositing amount and distributing referral incomes and calculating rewards", async function () {
+      await mockUsdc
+        .connect(owner)
+        .mint(signers[1].address, expandTo6Decimals(3000));
+      await mockUsdc
+        .connect(owner)
+        .mint(signers[2].address, expandTo6Decimals(3000));
+      await mockUsdc
+        .connect(owner)
+        .mint(signers[3].address, expandTo6Decimals(3000));
+      await mockUsdc
+        .connect(owner)
+        .mint(signers[4].address, expandTo6Decimals(3000));
+      await mockUsdc
+        .connect(owner)
+        .mint(signers[5].address, expandTo6Decimals(3000));
+      await mockUsdc
+        .connect(owner)
+        .mint(signers[6].address, expandTo6Decimals(3000));
 
+      await mockUsdc
+        .connect(owner)
+        .approve(myNetworking.address, expandTo6Decimals(3000));
+      await mockUsdc
+        .connect(signers[1])
+        .approve(myNetworking.address, expandTo6Decimals(3000));
+      await mockUsdc
+        .connect(signers[2])
+        .approve(myNetworking.address, expandTo6Decimals(3000));
+      await mockUsdc
+        .connect(signers[3])
+        .approve(myNetworking.address, expandTo6Decimals(3000));
+      await mockUsdc
+        .connect(signers[4])
+        .approve(myNetworking.address, expandTo6Decimals(3000));
+      await mockUsdc
+        .connect(signers[5])
+        .approve(myNetworking.address, expandTo6Decimals(3000));
+      await mockUsdc
+        .connect(signers[6])
+        .approve(myNetworking.address, expandTo6Decimals(3000));
 
-
-      
-      await myNetworking.connect(owner).deposit(1,zeroAddress,expandTo6Decimals(2000));
-      await myNetworking.connect(signers[1]).deposit(3,owner.address,expandTo6Decimals(1000));
-      await myNetworking.connect(signers[2]).deposit(2,signers[1].address,expandTo6Decimals(1000));
-      await myNetworking.connect(signers[3]).deposit(6,signers[2].address,expandTo6Decimals(1000));
-      await myNetworking.connect(signers[4]).deposit(1,signers[3].address,expandTo6Decimals(1000));
-      await myNetworking.connect(signers[5]).deposit(3,signers[4].address,expandTo6Decimals(1000));
-      await myNetworking.connect(signers[6]).deposit(1,signers[5].address,expandTo6Decimals(1000));
-
+      await myNetworking
+        .connect(owner)
+        .deposit(1, zeroAddress, expandTo6Decimals(2000));
+      await myNetworking
+        .connect(signers[1])
+        .deposit(3, owner.address, expandTo6Decimals(1000));
+      await myNetworking
+        .connect(signers[2])
+        .deposit(2, signers[1].address, expandTo6Decimals(1000));
+      await myNetworking
+        .connect(signers[3])
+        .deposit(6, signers[2].address, expandTo6Decimals(1000));
+      await myNetworking
+        .connect(signers[4])
+        .deposit(1, signers[3].address, expandTo6Decimals(1000));
+      await myNetworking
+        .connect(signers[5])
+        .deposit(3, signers[4].address, expandTo6Decimals(1000));
+      await myNetworking
+        .connect(signers[6])
+        .deposit(1, signers[5].address, expandTo6Decimals(1000));
 
       // console.log(await myNetworking.seeDeposit(signers[1].address,1));
       // console.log(await myNetworking.Details(owner.address));
 
-      await network.provider.send("evm_increaseTime", [12232000])
+      await network.provider.send("evm_increaseTime", [12232000]);
       await network.provider.send("evm_mine");
 
-      await myNetworking.connect(signers[6]).withdrawReward(expandTo6Decimals(200));
-      await myNetworking.connect(signers[6]).withdrawReward(expandTo6Decimals(160));
+      await myNetworking
+        .connect(signers[6])
+        .withdrawReward(expandTo6Decimals(200));
+      await myNetworking
+        .connect(signers[6])
+        .withdrawReward(expandTo6Decimals(160));
 
-      
-      await myNetworking.connect(signers[5]).withdrawReward(expandTo6Decimals(160));
-
-      
+      await myNetworking
+        .connect(signers[5])
+        .withdrawReward(expandTo6Decimals(160));
 
       // console.log("Referral Income",await myNetworking.referralIncome(owner.address));
       // console.log("Referral Income",await myNetworking.Details(signers[6].address));
-
-
-
     });
 
+    it("");
   });
-  });
+});
